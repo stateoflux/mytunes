4a954bc2bc0cb2adae8751a5519877bb66bcbfab
Wayne Montague <wayne.montague@gmail.com>
2013-11-27 12:13:49 -0800

    corrected mistakes made in spec files. files are calling out for App and Song but they are actually called AppModel and SongModel.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,7 +4,9 @@ window.MyTunes.Collections = window.MyTunes.Collections || {};
 
 MyTunes.Collections.SongQueue = MyTunes.Collections.Songs.extend({
 
+  model: MyTunes.Models.SongModel,
   initialize: function(){
+
   }
 
 });
